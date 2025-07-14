553e5f4e14103b49b217ee58cd0253dd4f8ec9b2
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-14 18:43:36 +0330

    Add CameraCard component to display camera information with status indicators and live image preview

---
+++ src/components/templates/CameraCard.js
@@ -0,0 +1,42 @@
+'use client'
+
+import Image from 'next/image';
+import React from 'react'
+import { AiOutlineExpand } from "react-icons/ai";
+import { BsFillCameraVideoFill } from "react-icons/bs";
+
+
+function CameraCard({ camera, onFullScreen }) {
+
+
+
+  return (
+    <>
+
+      <div className=' p-5  rounded-xl md:last:col-span-2 bg-gradient-to-tr from-[#1e293b] to-[#334155] shadow-xl'>
+        <div className='flex items-center justify-between'>
+          <div>
+            <p className='md:text-xl'>
+              {camera.name}
+            </p>
+            <span className='text-xs text-zinc-400'>{camera.locationRecord}</span>
+
+          </div>
+          <div className='flex items-center gap-x-2'>
+            <p className={`${camera.status === "offline" ? "bg-red-600" : " bg-green-500/70 "} animate-pulse px-1.5 py-0.5 rounded-md`}>
+              {camera.status}</p>
+            <AiOutlineExpand className='size-5'
+              onClick={onFullScreen} />
+          </div>
+
+        </div>
+        <div className='bg-slate-900 flex flex-col space-y-3 items-center justify-center rounded-xl w-full h-64 mt-2 mx-auto'>
+          <Image src={camera.liveImage} alt='camera' width={400} height={100} priority className='bg-cover  h-full rounded-md' />
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default CameraCard
