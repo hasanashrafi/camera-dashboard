8a459e9a26e7f347a3b63af8d7a680b0818db877
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-14 18:43:55 +0330

    Add ViewToggle component for full-screen display functionality

---
+++ src/components/templates/ViewToggle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ViewToggle() {
+    return (
+        <div className='w-full min-h-screen bg-slate-700 rounded-xl backdrop-blur-lg mx-auto'>
+            full screen
+        </div>
+    )
+}
+
+export default ViewToggle
