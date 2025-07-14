11b9f78e298f2329c1ecd355ac992ecfbb70c60e
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-14 18:43:24 +0330

    Add AlertCard component to display alert information with status indicators

---
+++ src/components/templates/AlertCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function AlertCard({ alert }) {
+
+    return (
+        <>
+
+            <div className='flex items-center justify-between p-2 rounded-lg bg-gradient-to-tr from-[#1e293b] to-[#334155] border border-[#475569] shadow-xl'>
+                <div>
+                    <p className='text-sm'>{alert.name}</p>
+                    <span className='text-xs text-gray-400'>{alert.location}</span>
+                    <p className='text-xs text-gray-400'>{alert.time.toLocaleString()}</p>
+                </div>
+                <p
+                    className={`${alert.status === "normal"
+                            ? "bg-yellow-500"
+                            : alert.status === "low"
+                                ? "bg-orange-500"
+                                : alert.status === "high"
+                                    ? "bg-red-500"
+                                    : ""
+                        } rounded-md py-1 px-3`}
+                >
+                    {alert.status}
+                </p>            </div>
+
+
+
+
+        </>
+    )
+}
+
+export default AlertCard
