b9ac794ce188083afa851321c2cf9e3378c31152
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-14 18:41:40 +0330

    Add NavBar component with status indicators for cameras, alerts, storage, and performance metrics

---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { PiSecurityCamera } from "react-icons/pi";
+import { MdOutlineStorage } from "react-icons/md";
+import { GoAlert } from "react-icons/go";
+import { FaChartLine } from "react-icons/fa6";
+
+
+function NavBar() {
+
+ 
+  return (
+    <div className='w-full flex flex-wrap items-center justify-center  p-2 gap-2 '>
+
+      <div className='flex items-center justify-center rounded-md p-2 bg-gradient-to-b from-slate-900 to-slate-700 w-1/3  md:w-[295px] h-24'>
+        <div className='flex items-center gap-x-2 justify-center text-sky-400 '>
+          <PiSecurityCamera className='size-7' />
+          <span className='text-2xl'>4/5</span>
+        </div>
+
+      </div>
+      <div className='flex items-center justify-center rounded-md p-2  bg-gradient-to-b from-slate-900 to-slate-700 w-1/3  md:w-[295px] h-24'>
+        <div className='flex items-center gap-x-2 justify-center text-yellow-400 animate-pulse '>
+          <GoAlert className='size-7' />
+          <span className='text-2xl'>3</span>
+        </div>
+
+      </div>
+      <div className='flex items-center justify-center rounded-md p-2  bg-gradient-to-b from-slate-900 to-slate-700 w-1/3  md:w-[295px] h-24'>
+        <div className='flex items-center gap-x-2 justify-center text-green-400 '>
+          <MdOutlineStorage className='size-7' />
+          <span className='text-2xl'>76%</span>
+        </div>
+
+      </div>
+
+      <div className='flex items-center justify-center rounded-md p-2  bg-gradient-to-b from-slate-900 to-slate-700 w-1/3  md:w-[295px] h-24'>
+        <div className='flex items-center gap-x-2 justify-center text-purple-400 '>
+          <FaChartLine className='size-7' />
+          <span className='text-2xl'>99.9%</span>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
