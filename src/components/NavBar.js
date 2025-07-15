d9cc121dabb99be730bf6e04daeaf5456b9719aa
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-15 12:06:10 +0330

    Integrate Link component in NavBar for navigation to alerts page, enhancing user interaction with alert notifications.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,12 @@ import { PiSecurityCamera } from "react-icons/pi";
 import { MdOutlineStorage } from "react-icons/md";
 import { GoAlert } from "react-icons/go";
 import { FaChartLine } from "react-icons/fa6";
+import Link from 'next/link';
 
 
 function NavBar() {
 
- 
+
   return (
     <div className='w-full flex flex-wrap items-center justify-center  p-2 gap-2 '>
 
@@ -18,11 +19,14 @@ function NavBar() {
         </div>
 
       </div>
-      <div className='flex items-center justify-center rounded-md p-2  bg-gradient-to-b from-slate-900 to-slate-700 w-1/3  md:w-[295px] h-24'>
-        <div className='flex items-center gap-x-2 justify-center text-yellow-400 animate-pulse '>
-          <GoAlert className='size-7' />
-          <span className='text-2xl'>3</span>
-        </div>
+     
+      <div className=' flex items-center justify-center rounded-md p-2   bg-gradient-to-b from-slate-900  to-slate-700 w-1/3  md:w-[295px] h-24'>
+        <Link href="/alerts">
+          <div className=' flex items-center gap-x-2 justify-center text-yellow-400 animate-pulse '>
+            <GoAlert className='size-7' />
+            <span className='text-2xl'>3</span>
+          </div>
+        </Link>
 
       </div>
       <div className='flex items-center justify-center rounded-md p-2  bg-gradient-to-b from-slate-900 to-slate-700 w-1/3  md:w-[295px] h-24'>
